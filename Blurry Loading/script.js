8f83b77ac5a03439d9835f9c95dbfacb5bf4b6c8
Neivanh <sonix88@list.ru>
2022-11-29 17:01:43 +0600

    r

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -17,7 +17,6 @@ function blurring() {
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
