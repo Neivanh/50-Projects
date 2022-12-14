8cf46ef1cdaa5f02b7869d66b3d0daed0759fa27
Neivanh <sonix88@list.ru>
2022-12-14 18:31:46 +0600

    Animated_Navigation

--- Sound Board/script.js
+++ Sound Board/script.js
@@ -1,25 +1,25 @@
-let sounds = ['play1', 'play2', 'play3', 'play4', 'play5', 'play6']
+let sounds = ["play1", "play2", "play3", "play4", "play5", "play6"];
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    btn.innerText = sound
+  btn.innerText = sound;
 
-    btn.addEventListener('click', () => {
-        stopSongs()
+  btn.addEventListener("click", () => {
+    stopSongs();
 
-        document.getElementById(sound).play()
-    })
+    document.getElementById(sound).play();
+  });
 
-    document.getElementById('buttons').appendChild(btn)
-})
+  document.getElementById("buttons").appendChild(btn);
+});
 
 function stopSongs() {
-    sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
 
-        song.pause()
-        song.currentTime = 0;
-    })
+    song.pause();
+    song.currentTime = 0;
+  });
 }
